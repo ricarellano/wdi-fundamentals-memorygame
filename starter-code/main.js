a56a397309e65dd5a8e425a65697bfe4c74377ae
Ricardo Arellano <rarellano@Ricardos-MacBook-Pro-2.local>
2016-11-02 21:11:36 -0600

    unit 9

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,9 @@ var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "jack";
 var cardFour = "ace";
+
+if (cardTwo === cardFour) {
+   alert("Sorry, try again.");
+} else {
+	alert("You found a match!");
+}
