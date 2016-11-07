27cb7e8e578bf99c64c9425316e18dd3dfcd378d
Ricardo Arellano <rarellano@Ricardos-MacBook-Pro-2.local>
2016-11-07 00:16:54 -0600

    unit 10

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,11 +1,22 @@
-console.log("JS file is connected to HTML! Woo!")
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "jack";
-var cardFour = "ace";
+// console.log("JS file is connected to HTML! Woo!")
+// var cardOne = "queen";
+// var cardTwo = "king";
+// var cardThree = "jack";
+// var cardFour = "ace";
+var cards = ['queen', 'queen', 'king', 'king'];
+// if (cardTwo === cardFour) {
+//    alert("Sorry, try again.");
+// } else {
+// 	alert("You found a match!");
+// }
+var gameBoard = document.getElementById('game-board');
 
-if (cardTwo === cardFour) {
-   alert("Sorry, try again.");
-} else {
-	alert("You found a match!");
+var createCards = function(){
+  for (var i = 0; i < cards.length; i++) {
+  	var cardElement = document.createElement('div');
+  	    cardElement.className = 'card';
+  	    gameBoard.appendChild(cardElement);
+  }
 }
+
+createCards();
